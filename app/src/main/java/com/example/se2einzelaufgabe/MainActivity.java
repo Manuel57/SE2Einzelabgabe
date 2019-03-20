9b3d211c7e7d10a331d86e2ce5ac8198b6732719
Manuel57 <manuel.lackenbucher.gmail.com>
2019-03-20 11:28:15 +0100

    create view

--- app/src/main/java/com/example/se2einzelaufgabe/MainActivity.java
+++ app/src/main/java/com/example/se2einzelaufgabe/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.se2einzelaufgabe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnMatNr = findViewById(R.id.btnMatNr);
+        btnMatNr.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnMatNr) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            });
+        }
     }
 }
